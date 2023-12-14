ffc344557f7400dd3d585f824dd10d4a6b96f82d
tyVespA <migliavacca.marco08@gmail.com>
2023-12-14 23:05:55 +0100

    Add models

---
+++ models/bookinstance.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BookInstanceSchema = new Schema({
+  // reference to the associated book
+  book: { type: Schema.Types.ObjectId, ref: "Book", required: true },
+  imprint: { type: String, required: true },
+  status: {
+    type: String,
+    required: true,
+    enum: ["Available", "Maintenance", "Loaned", "Reserve"],
+    default: "Maintenance",
+  },
+  due_back: { type: Date, default: Date.now },
+});
+
+// Vortual for bookinstance's URL
+BookInstanceSchema.virtual("url").get(function () {
+  return `/catalog/bookinstance/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("BookInstance", BookInstanceSchema);
