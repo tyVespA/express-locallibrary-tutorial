cf78a4aba3f7f39619be9bb673deebe1c6c226e4
tyVespA <migliavacca.marco08@gmail.com>
2023-12-14 23:05:46 +0100

    Set up db connection

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 
+// Setup mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB =
+  "mongodb+srv://mmpotto:YTTbgrEsiCbYWnrF@cluster0.oierxdg.mongodb.net/local_library?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
